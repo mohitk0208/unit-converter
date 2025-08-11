0062b98efc042a0755cdff535cc75a11d2120858
Mohit kumar <mohitk02082000@gmail.com>
2025-08-11 23:57:16 +0530

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+// import LenghtConverter from "./components/LengthConverter"
+
+
+function App() {
+
+  return (
+    <div className='w-full h-full p-5 '>
+      <h1 className="text-4xl font-bold my-1">Unit Converter</h1>
+
+      <div className="my-2">
+        List the converters here...
+
+        {/* <LenghtConverter /> */}
+      </div>
+
+    </div>
+  )
+}
+
+export default App
